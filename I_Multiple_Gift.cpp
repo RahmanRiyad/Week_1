051c2467441e751752aef2bab049058b58ba768d
NaiemUrRahmanRiyad <163662457+NaiemUrRahmanRiyad@users.noreply.github.com>
2024-03-17 19:28:06 +0600

    Create I_Multiple_Gift.cpp

---
+++ I_Multiple_Gift.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int x, y;
+    cin >> x >> y;
+    int count = 0;
+    while (x <= y)
+    {
+        x = x * 2;
+        count++;
+    }
+    cout << count << '\n';
+    return 0;
+}
